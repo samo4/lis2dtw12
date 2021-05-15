9fa41e526e7dd47790c4e5bf9b45f0cc4b4111bc
Samo 4 <samo.4tuna@gmail.com>
2021-05-15 14:51:00 +0200

    fixing communication issues

--- lis2dtw12_i2c.c
+++ lis2dtw12_i2c.c
@@ -20,25 +20,85 @@
 
 static uint16_t lis2dtw12_i2c_slave_addr = DT_INST_REG_ADDR(0);
 
+#define HAL_I2C_TX_MAX_LEN (128)
+
 LOG_MODULE_DECLARE(LIS2DTW12, CONFIG_SENSOR_LOG_LEVEL);
 
-static int lis2dtw12_i2c_read(struct lis2dtw12_data *data, uint8_t reg_addr,
-				 uint8_t *value, uint16_t len)
-{
-	return i2c_burst_read(data->bus, lis2dtw12_i2c_slave_addr,
-			      reg_addr, value, len);
+int hal_i2c_write(struct lis2dtw12_data *data, uint8_t addr, uint8_t reg, uint8_t *tx_buf, uint8_t tx_len)
+	{
+	if (!data->bus) {
+		LOG_ERR("Device is not ready. Please init");
+		return -1;
+	}
+
+	if (addr < 3 || addr > 0x77) {
+		LOG_ERR("The address MUST be in range [0x04 .. 0x77]");
+		return -1;
+	}
+
+	if (tx_len + 1 > HAL_I2C_TX_MAX_LEN) {
+		LOG_ERR("Transmit len is out of range [0 to 128]");
+		return -1;
+	}
+
+	uint8_t txbuf[HAL_I2C_TX_MAX_LEN] = {0x00};
+	txbuf[0] = reg;
+	memcpy(&txbuf[1], tx_buf, tx_len);
+	if (i2c_write(data->bus, txbuf, tx_len + 1, addr) != 0) {
+		return -1;
+	}
+
+	return 0;
 }
 
-static int lis2dtw12_i2c_write(struct lis2dtw12_data *data, uint8_t reg_addr,
-				  uint8_t *value, uint16_t len)
+
+int hal_i2c_read(struct lis2dtw12_data *data, uint8_t addr, uint8_t reg, uint8_t *rx_buf, uint8_t rx_len)
 {
-	return i2c_burst_write(data->bus, lis2dtw12_i2c_slave_addr,
-			       reg_addr, value, len);
+	if (!data->bus) {
+	  LOG_ERR("Device is not ready. Please init");
+	  return -1;
+	}
+
+	if (addr < 3 || addr > 0x77) {
+	  LOG_ERR("The address MUST be in range [0x04 .. 0x77]");
+	  return -1;
+	}
+
+	uint8_t txbuf[1] = {reg};
+	if (i2c_write_read(data->bus, addr, txbuf, 1, rx_buf, rx_len) != 0) {
+		return -1;
+	}
+  return 0;
 }
+
+
+// static int lis2dtw12_i2c_read(struct lis2dtw12_data *data, uint8_t reg_addr,
+// 				 uint8_t *value, uint16_t len)
+// {
+// 	return i2c_burst_read(data->bus, lis2dtw12_i2c_slave_addr,
+// 			      reg_addr, value, len);
+// }
+
+// static int lis2dtw12_i2c_write(struct lis2dtw12_data *data, uint8_t reg_addr,
+// 				  uint8_t *value, uint16_t len)
+// {
+// 	return i2c_burst_write(data->bus, lis2dtw12_i2c_slave_addr, reg_addr, value, len);
+// }
+
+int32_t sensor_write_func(struct lis2dtw12_data *data, uint8_t reg, uint8_t *value, uint16_t len)
+{
+  return hal_i2c_write(data, lis2dtw12_i2c_slave_addr, reg, value, (uint16_t)len);
+}
+
+int32_t sensor_read_func(struct lis2dtw12_data *data, uint8_t reg, uint8_t *value, uint16_t len)
+{
+  return hal_i2c_read(data, lis2dtw12_i2c_slave_addr, reg, value, (uint16_t)len);
+}
+
 
 stmdev_ctx_t lis2dtw12_i2c_ctx = {
-	.read_reg = (stmdev_read_ptr) lis2dtw12_i2c_read,
-	.write_reg = (stmdev_write_ptr) lis2dtw12_i2c_write,
+	.read_reg = (stmdev_read_ptr) sensor_read_func,
+	.write_reg = (stmdev_write_ptr) sensor_write_func,
 };
 
 int lis2dtw12_i2c_init(const struct device *dev)
